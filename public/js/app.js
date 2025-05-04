a982c1f608949816d4e0f3b5a079be3bef95cb3f
alirezaabbasi-dev <alirezaabbasi001@outlook.com>
2025-05-04 22:47:58 +0330

    responsive header and main && redesign somethings ...

--- public/js/app.js
+++ public/js/app.js
@@ -17,9 +17,9 @@ let getDataFromAPI = fetch("../src/data.json")
 
         `
         <!-- item start -->
-        <div class="max-w-75 h-85  bg-slate-600 rounded-xl shadow-md shadow-slate-700">
+        <div class="max-w-160 h-85  bg-slate-600 rounded-xl shadow-md shadow-slate-700">
             <div class="h-[70%] bg-white rounded-2xl overflow-hidden">
-                <img loading="lazy" class="block w-full h-full object-cover text-center" src="${
+                <img loading="lazy" class="block sm:w-full h-full object-contain md:object-cover  mx-auto" src="${
                   item.src
                 }" alt="">
             </div>
