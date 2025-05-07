501f70f8911671c6ae2567f7d32d9485852479dc
Kiyan Abbasi <alirezaabbasi001@outlook.com>
2025-05-08 01:42:10 +0330

    redesigned "footer" and "main" contents

--- public/js/app.js
+++ public/js/app.js
@@ -17,16 +17,18 @@ let getDataFromAPI = fetch("../src/data.json")
 
         `
         <!-- item start -->
-        <div class="max-w-160 h-85  bg-slate-600 rounded-xl shadow-md shadow-slate-700">
+        <div class="max-w-160 h-85 rounded-xl shadow-md bg-gradient-to-t from-slate-800 to-slate-700 shadow-slate-800 border-1 border-slate-600 ">
             <div class="h-[70%] bg-white rounded-2xl overflow-hidden">
                 <img loading="lazy" class="block sm:w-full h-full object-contain md:object-cover  mx-auto" src="${
                   item.src
                 }" alt="">
             </div>
-            <div class="px-4 text-center" >
-                <h5 class="">${item.name}</h5>
-                <p id="paragraph">${
-                  item.level ? "level : " + item.level : ""
+            <div class="px-4 text-center mt-4" >
+                <h5 class="font-inter text-xl text-slate-300 font-bold tracking-wider">${
+                  item.name
+                }</h5>
+                <p id="paragraph" class="text-sm text-slate-400 mt-2">${
+                  item.level ? "Level : " + item.level : ""
                 }</p>
 
             </div>
